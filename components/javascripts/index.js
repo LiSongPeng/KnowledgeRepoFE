dc1131b6ac0a58cd14108e50343588741268cb21
JChang07 <guaishou07@yeah.net>
2017-08-30 11:24:29 +0800

    前端页面搭建完成。

--- components/javascripts/index.js
+++ components/javascripts/index.js
@@ -1,3 +1,17 @@
 /**
  * Created by Letg4 on 2017/8/28.
  */
+var indexapp=angular.module('indexapp',['ui.router','globalconfig']);
+var stateProvider=null;
+indexapp.config(function ($stateProvider,$urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'home.html'
+        });
+    $urlRouterProvider.otherwise('/home');
+    stateProvider=$stateProvider;
+});
+indexapp.controller('mainCtrl',mainController);
+indexapp.controller('navListCtrl',['$scope',navController]);
+indexapp.controller('breadcrumbCtrl',['$scope',breadcrumbCtrl]);
