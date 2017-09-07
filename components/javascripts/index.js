ab32b1a5009662a5a340e6de3bf8ae9813b2feb7
john19961212 <1442364096@qq.com>
2017-09-07 16:47:26 +0800

    添加退出操作

--- components/javascripts/index.js
+++ components/javascripts/index.js
@@ -1,8 +1,8 @@
 /**
  * Created by Letg4 on 2017/8/28.
  */
-var indexapp=angular.module('indexapp',['mainRouter','globalconfig']);
-indexapp.config(function ($stateProvider,$urlRouterProvider) {
+var indexapp = angular.module('indexapp', ['mainRouter', 'globalconfig']);
+indexapp.config(function ($stateProvider, $urlRouterProvider) {
     // $stateProvider
     //     .state('home', {
     //         url: '/home',
@@ -10,7 +10,14 @@ indexapp.config(function ($stateProvider,$urlRouterProvider) {
     //     });
     // $urlRouterProvider.otherwise('/home');
 });
-indexapp.controller('mainCtrl',mainController);
+indexapp.controller('navController', ['$scope', function ($scope) {
+    $scope.currUser = JSON.parse(window.sessionStorage.getItem('currUser'));
+    $scope.quit = function () {
+        window.sessionStorage.setItem("currUser", null);
+        window.location.href = "login.html";
+    };
+}]);
+indexapp.controller('mainCtrl', mainController);
 // indexapp.controller('loginboxCtrl',loginboxController);
-indexapp.controller('navListCtrl',['$scope','$rootScope',navController]);
-indexapp.controller('breadcrumbCtrl',['$scope',breadcrumbCtrl]);
+indexapp.controller('navListCtrl', ['$scope', '$rootScope', navController]);
+indexapp.controller('breadcrumbCtrl', ['$scope', breadcrumbCtrl]);
