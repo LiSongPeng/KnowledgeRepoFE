a15c7bf2b5aab25d13f0dec882d6d05d5743c485
john19961212 <1442364096@qq.com>
2017-09-07 20:37:51 +0800

    修改详情页

--- components/javascripts/res_js/detail.js
+++ components/javascripts/res_js/detail.js
@@ -14,14 +14,29 @@ var detailController = detail.controller("detailController", ["$scope", "$http",
     $scope.getTrustedHtml = function (html) {
         return $sce.trustAsHtml(html);
     };
+    Date.prototype.Format = function (fmt) { //author: meizz
+        var o = {
+            "M+": this.getMonth() + 1, //月份
+            "d+": this.getDate(), //日
+            "h+": this.getHours(), //小时
+            "m+": this.getMinutes(), //分
+            "s+": this.getSeconds(), //秒
+            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+            "S": this.getMilliseconds() //毫秒
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    }
     $http({
         method: "GET",
         url: "http://localhost:8080/knowledgeRepo/repo/getKnowledgeDetail.form?id=" + detailId,
     }).then(function successCallback(response) {
         if (response.data.flag == 200) {
             $scope.response = response.data.data;
-            $scope.response.createTime = new Date($scope.response.createTime).toLocaleString();
-            $scope.response.kApprTime = new Date($scope.response.kApprTime).toLocaleString();
+            $scope.response.createTime = new Date($scope.response.createTime).Format("yyyy-MM-dd hh:mm:ss");
+            $scope.response.kApprTime = new Date($scope.response.kApprTime).Format("yyyy-MM-dd hh:mm:ss");
         } else {
             toastr.info("未查找到结果");
         }
