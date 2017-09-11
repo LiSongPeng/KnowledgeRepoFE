90685c1583a4a9f1556bfeb59779287e821d84d2
JChang07 <guaishou07@yeah.net>
2017-09-11 15:25:22 +0800

    修改主页

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require ('express') ;
 var path = require('path');
 var app = express() ;
+// var router = require('./routes/index');
 
 app.use(express.static (path.join(__dirname, 'components')));
 app.use(express.static (path.join(__dirname, 'bower_components')));
 app.set ('port', process.env.PORT || 14250) ;
 app.use(express.static(path.join(__dirname, 'tpls')));
-console.log(__dirname);
-app.use(function (req , res){
-    res.sendfile('./tpls/login.html')
+
+app.use('/',function (req,res,next) {
+    res.sendfile("./tpls/index.html");
 });
 module.exports = app ;
